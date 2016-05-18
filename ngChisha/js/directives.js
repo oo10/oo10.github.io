05272bde9d6f13c966f0517cc885d199fcdbbf54
oo10 <simple11@vip.qq.com>
2016-05-18 22:39:03 +0800

    吃啥呢NgWebApp

---
+++ ngChisha/js/directives.js
@@ -0,0 +1,15 @@
+var bookStoreDirectives = angular.module('bookStoreDirectives', []);
+
+bookStoreDirectives.directive('pageBg', function() {
+        return {
+            //restrict: "E",
+            //template : "<h1>定义指令!</h1>"
+        };
+});
+
+//
+//bookStoreDirectives.directive('bookStoreDirective_1', ['$scope',
+//    function($scope) {
+//
+//    }
+//]);
